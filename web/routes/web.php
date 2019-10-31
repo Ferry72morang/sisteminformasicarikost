7a77ff950cf46cd6ac813019ec0924b7377b3e2a
ferrysitumorang <ferrypriantositumorangcr7@gmail.com>
2019-10-31 15:22:20 +0700

    Membuat fitur Login Register

--- web/routes/web.php
+++ web/routes/web.php
@@ -12,5 +12,21 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.login');
 });
+
+Auth::routes();
+
+Route::middleware("auth")->group(function(){
+Route::get('home', function () {
+    return view('pages.home');
+})->name("home");
+});
+
+Route::get('register', function () {
+    return view('pages.register');
+})->name("register")->middleware("guest");
+
+Route::get('login', function () {
+    return view('pages.login');
+})->name("login")->middleware("guest");
