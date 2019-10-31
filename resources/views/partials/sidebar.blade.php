18f4ade8c10a5904d0f2a8e95151cf96c0c92d35
ferrysitumorang <ferrypriantositumorangcr7@gmail.com>
2019-10-31 10:11:38 +0700

    Membuat fitur Login Register baru

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -32,7 +32,6 @@
                          <i class="nav-icon fas fa-tachometer-alt"></i>
                          <p>
                              Pesan
-                            
                          </p>
                      </a>
 
